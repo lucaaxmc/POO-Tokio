7c7df164cc467fb51887915e1c1e40e918fe6ca6
Luca <133663019+lucaaxmc@users.noreply.github.com>
2023-05-15 14:26:42 +0100

    Add files via upload

---
+++ módulo_2/Prática 6/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        exercicio6_Tabuada.exercicio6();
+    }
+}
---
+++ módulo_2/Prática 6/exercicio6_Tabuada.java
@@ -0,0 +1,7 @@
+public class exercicio6_Tabuada {
+    public static void exercicio6() {
+        int n = 9;
+        for (int i = 1; i <= 10; ++i)
+            System.out.println(n + " * " + i + " = " + n * i);
+    }
+}
